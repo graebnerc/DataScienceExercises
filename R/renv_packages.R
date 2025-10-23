f6a91f7911b1e5dd59bb7255ad44a28d7e7dae17
Claudius Graebner-Radkowitsch <claudius@claudius-graebner.com>
2025-10-23 17:31:18 +0200

    Deactivate renv for now because of inconsistencies

--- R/renv_packages.R
+++ R/renv_packages.R
@@ -10,9 +10,11 @@ library(dplyr)
 library(gradethis)
 library(ggcheck)
 
-v_1 <- 1:4
-v_2 <- letters[1:4]
-v_3 <- c(TRUE, FALSE, TRUE, FALSE)
-tib_1 <- tibble("vec_1"=v_1, "vec_2"=v_2, "vec_3"=v_3)
-dplyr::glimpse(tib_1)
-stringr::str_detect("b", v_2)
+if (FALSE){
+  v_1 <- 1:4
+  v_2 <- letters[1:4]
+  v_3 <- c(TRUE, FALSE, TRUE, FALSE)
+  tib_1 <- tibble("vec_1"=v_1, "vec_2"=v_2, "vec_3"=v_3)
+  dplyr::glimpse(tib_1)
+  stringr::str_detect("b", v_2)
+}
